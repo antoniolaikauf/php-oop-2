c37a2913ce251070c10dc8574a7b46cfb668c226
antoniolaikauf <antonio.laikauf00@gmail.com>
2024-02-05 14:45:15 +0100

    add: aggiunto classe prodotti

--- index.php
+++ index.php
@@ -1,2 +1,53 @@
 <?php
-echo 'funziono';
+// classe con dentro dei tipi di prodotti
+class prodotti
+{
+    private $cibo;
+    private $giochi;
+    private $cuccie;
+
+    public function __construct(
+        $cibo,
+        $giochi,
+        $cuccie
+    ) {
+
+        $this->setcibo($cibo);
+        $this->setgiochi($giochi);
+        $this->setcuccie($cuccie);
+    }
+
+    // metodi per cambiare le proprietà private
+    public function getcibo()
+    {
+
+        return $this->cibo;
+    }
+    public function setcibo($cibo)
+    {
+
+        $this->cibo = $cibo;
+    }
+    public function getgiochi()
+    {
+
+        return $this->giochi;
+    }
+    public function setgiochi($giochi)
+    {
+
+        $this->giochi = $giochi;
+    }
+    public function getcuccie()
+    {
+
+        return $this->cuccie;
+    }
+    public function setcuccie($cuccie)
+    {
+
+        $this->cuccie = $cuccie;
+    }
+}
+$prodotti = new prodotti('carne', 'pupazzo', "cuccia all'aperto");
+var_dump($prodotti);
