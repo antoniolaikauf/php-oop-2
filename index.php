e6cb38633a17f3df2bbb2cd60bb8f6a75173cbdb
antoniolaikauf <antonio.laikauf00@gmail.com>
2024-02-05 15:11:46 +0100

    add: aggiunto classe prodottosingolo

--- index.php
+++ index.php
@@ -1,20 +1,71 @@
 <?php
 // classe con dentro dei tipi di prodotti
-class prodotti
+// caratteristiche prodotto singolo
+class prodottosingolo
+{
+    private $immagine;
+    private $prezzo;
+    private $nome;
+
+    public function __construct(
+        $immagine,
+        $prezzo,
+        $nome
+    ) {
+        $this->setimmagine($immagine);
+        $this->setprezzo($prezzo);
+        $this->setnome($nome);
+
+    }
+    public function getimmagine()
+    {
+
+        return $this->immagine;
+    }
+    public function setimmagine($immagine)
+    {
+
+        $this->immagine = $immagine;
+    }
+    public function getprezzo()
+    {
+
+        return $this->prezzo;
+    }
+    public function setprezzo($prezzo)
+    {
+
+        $this->prezzo = $prezzo;
+    }
+    public function getnome()
+    {
+
+        return $this->nome;
+    }
+    public function setnome($nome)
+    {
+
+        $this->nome = $nome;
+    }
+}
+class prodotti 
 {
     private $cibo;
     private $giochi;
     private $cuccie;
+    private $categoria;
 
     public function __construct(
         $cibo,
         $giochi,
-        $cuccie
+        $cuccie,
+        $categoria,
     ) {
 
         $this->setcibo($cibo);
         $this->setgiochi($giochi);
         $this->setcuccie($cuccie);
+        $this->setcategoria($categoria);
     }
 
     // metodi per cambiare le proprietà private
@@ -48,6 +99,21 @@ class prodotti
 
         $this->cuccie = $cuccie;
     }
+    public function getcategoria()
+    {
+
+        return $this->categoria;
+    }
+    public function setcategoria($categoria)
+    {
+
+        $this->categoria = $categoria;
+    }
 }
-$prodotti = new prodotti('carne', 'pupazzo', "cuccia all'aperto");
+
+
+$prodotti = new prodotti('carne', 'pupazzo', "cuccia all'aperto", 'cane');
+$prodotto = new prodottosingolo('immagine', '50 $', 'casagiocattolo');
 var_dump($prodotti);
+// echo "<br><br>";
+// var_dump($prodotto);
